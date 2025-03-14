0da79cf233745daf25e4b6e46b33453ea2be8149
Arantis_jr <arantisjunior@gmail.com>
2025-03-14 15:41:02 +0100

    correction

--- QUESTION 4/palindrome_checker.js
+++ QUESTION 4/palindrome_checker.js
@@ -1,7 +1,9 @@
 //A palindrome is a word or phrase which when reversed still read thesame 
 function palindrom(input){
     //removing spacing and convert all xters to lower case 
-    let char_lower = input.replace(/[A-Za-z0-9]/g,'').toLowerCase();
+    let char_lower = input.replace(" ",'').toLowerCase();
+    console.log(char_lower);
+    
     //reversing the char_lower
     let char_reversed = char_lower.split('').reverse().join('');
 
